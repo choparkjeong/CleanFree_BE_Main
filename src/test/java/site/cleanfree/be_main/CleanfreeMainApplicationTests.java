9471d780f144039bf6892fa6d463fda9a1762096
Chanwoong Park <128444378+chanchanwoong@users.noreply.github.com>
2024-07-23 02:04:47 +0900

    chore: 프로젝트 배포 셋팅

---
+++ src/test/java/site/cleanfree/be_main/CleanfreeMainApplicationTests.java
@@ -0,0 +1,13 @@
+package site.cleanfree.be_main;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CleanfreeMainApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
