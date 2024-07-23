be19c4231eefa31977b16e5528302fb345b03ac0
Chanwoong Park <128444378+chanchanwoong@users.noreply.github.com>
2024-07-23 15:36:59 +0900

    chore: errorCode 추가

--- src/main/java/site/cleanfree/be_main/common/BaseResponse.java
+++ src/main/java/site/cleanfree/be_main/common/BaseResponse.java
@@ -6,12 +6,14 @@ import lombok.Getter;
 @Getter
 public class BaseResponse<T> {
     private final boolean success;
+    private final int errorCode; // success = true 인 경우, 1000
     private final String message;
     private final T data;
 
     @Builder
-    public BaseResponse(boolean success, String message, T data) {
+    public BaseResponse(boolean success, int errorCode, String message, T data) {
         this.success = success;
+        this.errorCode = errorCode;
         this.message = message;
         this.data = data;
     }
