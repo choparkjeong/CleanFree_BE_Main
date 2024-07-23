457824118ec6e15b77c77ce78b64ec12714b56f5
Chanwoong Park <128444378+chanchanwoong@users.noreply.github.com>
2024-07-23 15:46:59 +0900

    feat: 소셜 로그인을 위한 엔티티 생성

--- src/main/java/site/cleanfree/be_main/auth/domain/SnsInfo.java
+++ src/main/java/site/cleanfree/be_main/auth/domain/SnsInfo.java
@@ -1,4 +1,31 @@
 package site.cleanfree.be_main.auth.domain;
 
+import jakarta.persistence.*;
+import lombok.AccessLevel;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Getter
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
 public class SnsInfo {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "sns_info_id")
+    private Long id;
+    @Column(name = "sns_type", nullable = false)
+    private String snsType;
+    @Column(name = "sns_id", nullable = false)
+    private String snsId;
+    @ManyToOne(fetch = FetchType.LAZY)
+    private Member member;
+
+    @Builder
+    public SnsInfo(String snsType, String snsId, Member member) {
+        this.snsType = snsType;
+        this.snsId = snsId;
+        this.member = member;
+    }
 }
