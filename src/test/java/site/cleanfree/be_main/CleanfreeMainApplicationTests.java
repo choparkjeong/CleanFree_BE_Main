7795fe5a9220671a3c0b872f41dd8c27eddaf4bb
Chanwoong Park <128444378+chanchanwoong@users.noreply.github.com>
2024-07-23 13:11:49 +0900

    chore: 배포에 필요한 파일 수정

--- src/test/java/site/cleanfree/be_main/CleanfreeMainApplicationTests.java
+++ src/test/java/site/cleanfree/be_main/CleanfreeMainApplicationTests.java
@@ -3,7 +3,7 @@ package site.cleanfree.be_main;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
-@SpringBootTest
+//@SpringBootTest
 class CleanfreeMainApplicationTests {
 
 	@Test
