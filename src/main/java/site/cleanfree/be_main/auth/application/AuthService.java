9dfd787ec755a6f1ab61ee7517c7d96eddf004b4
ddolboghi <rhdfyd128@gmail.com>
2024-08-01 22:25:48 +0900

    feat: 회원가입 API 구현 & 로그인 로직 수정

--- src/main/java/site/cleanfree/be_main/auth/application/AuthService.java
+++ src/main/java/site/cleanfree/be_main/auth/application/AuthService.java
@@ -2,7 +2,11 @@ package site.cleanfree.be_main.auth.application;
 
 import site.cleanfree.be_main.auth.dto.MemberSnsLoginRequestDto;
 import site.cleanfree.be_main.auth.dto.TokenResponseDto;
+import site.cleanfree.be_main.auth.vo.MemberSignupRequestVo;
 
 public interface AuthService {
+
     TokenResponseDto snsLogin(MemberSnsLoginRequestDto memberSnsLoginRequestDto);
+
+    TokenResponseDto signup(MemberSignupRequestVo memberSignupRequestVo);
 }
--- src/main/java/site/cleanfree/be_main/auth/domain/Member.java
+++ src/main/java/site/cleanfree/be_main/auth/domain/Member.java
@@ -1,11 +1,15 @@
 package site.cleanfree.be_main.auth.domain;
 
-import jakarta.persistence.*;
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
 import lombok.AccessLevel;
 import lombok.Builder;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
-import site.cleanfree.be_main.auth.dto.MemberSnsLoginRequestDto;
+import site.cleanfree.be_main.auth.vo.MemberSignupRequestVo;
 
 @Entity
 @Getter
@@ -16,34 +20,34 @@ public class Member {
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     @Column(name = "member_id")
     private Long id;
-//    @Column(name = "email", nullable = false, length = 30)
-//    private String email;
-//    @Column(name = "name", nullable = false, length = 20)
-//    private String name;
-//    @Column(name = "phone_num", nullable = false, length = 20)
-//    private String phoneNum;
-//    @Column(name = "sex", nullable = false, length = 20)
-//    private String sex;
-    @Column(name = "uuid", nullable = false)
+    @Column(name = "email", nullable = false, length = 50)
+    private String email;
+    @Column(name = "name", nullable = false, length = 50)
+    private String name;
+    @Column(name = "gender", nullable = false, length = 20)
+    private String gender;
+    @Column(name = "uuid", nullable = false, length = 10)
     private String uuid;
+    @Column(name = "age", nullable = false, length = 3)
+    private String age;
 
     @Builder
-    public Member(Long id, String email, String name, String phoneNum, String sex, String uuid) {
+    public Member(Long id, String email, String name, String gender, String uuid, String age) {
         this.id = id;
-//        this.email = email;
-//        this.name = name;
-//        this.phoneNum = phoneNum;
-//        this.sex = sex;
+        this.email = email;
+        this.name = name;
+        this.gender = gender;
         this.uuid = uuid;
+        this.age = age;
     }
 
-    public static Member converter(MemberSnsLoginRequestDto memberSnsLoginRequestDto) {
+    public static Member converter(MemberSignupRequestVo memberSignupRequestVo, String memberUuid) {
         return Member.builder()
-//                .email(memberSnsLoginRequestDto.getEmail())
-//                .name(memberSnsLoginRequestDto.getName())
-//                .phoneNum(memberSnsLoginRequestDto.getPhoneNumber())
-//                .sex(memberSnsLoginRequestDto.getSex())
-                .uuid(memberSnsLoginRequestDto.getUuid())
-                .build();
+            .email(memberSignupRequestVo.getEmail())
+            .name(memberSignupRequestVo.getName())
+            .gender(memberSignupRequestVo.getGender())
+            .age(memberSignupRequestVo.getAge())
+            .uuid(memberUuid)
+            .build();
     }
 }
