aabca6acbf3a82503433fd2c4840e1818182f6f7
ddolboghi <rhdfyd128@gmail.com>
2024-08-03 17:07:01 +0900

    fix: serialize 위한 annotation 추가

--- src/main/java/site/cleanfree/be_main/recommendation/domain/Reference.java
+++ src/main/java/site/cleanfree/be_main/recommendation/domain/Reference.java
@@ -1,7 +1,13 @@
 package site.cleanfree.be_main.recommendation.domain;
 
 import java.util.List;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.ToString;
 
+@NoArgsConstructor
+@Getter
+@ToString
 public class Reference {
 
     private List<String> youtube;
--- src/main/java/site/cleanfree/be_main/recommendation/domain/Review.java
+++ src/main/java/site/cleanfree/be_main/recommendation/domain/Review.java
@@ -1,5 +1,12 @@
 package site.cleanfree.be_main.recommendation.domain;
 
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.ToString;
+
+@NoArgsConstructor
+@Getter
+@ToString
 public class Review {
 
     private String score;
