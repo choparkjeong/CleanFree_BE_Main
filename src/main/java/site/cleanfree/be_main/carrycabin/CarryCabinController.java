d6879d3ec3a6cc573208af3dbde9ac0ce6acfc65
ddolboghi <rhdfyd128@gmail.com>
2024-08-10 18:44:28 +0900

    fix: carry cabin 사전신청 API 구현

---
+++ src/main/java/site/cleanfree/be_main/carrycabin/CarryCabinController.java
@@ -0,0 +1,30 @@
+package site.cleanfree.be_main.carrycabin;
+
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.tags.Tag;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+import site.cleanfree.be_main.common.BaseResponse;
+import site.cleanfree.be_main.cookingstation.CookingStationRegisterRequestVo;
+
+@RestController
+@RequestMapping("/api/v1/carrycabin")
+@RequiredArgsConstructor
+@Tag(name = "carrycabin API", description = "여행 가방 배달 서비스 API")
+public class CarryCabinController {
+
+    private final CarryCabinService carryCabinService;
+
+    @PostMapping("/register")
+    @Operation(summary = "사전 신청 API", description = "ip 주소를 기준으로 사전 신청을 저장합니다.")
+    public ResponseEntity<BaseResponse<Object>> search(
+        @RequestBody CarryCabinRegisterRequestVo carryCabinRegisterRequestVo
+    ) {
+        return ResponseEntity.ok(carryCabinService.register(carryCabinRegisterRequestVo));
+    }
+
+}
---
+++ src/main/java/site/cleanfree/be_main/carrycabin/CarryCabinRegisterRequestVo.java
@@ -0,0 +1,9 @@
+package site.cleanfree.be_main.carrycabin;
+
+import lombok.Getter;
+
+@Getter
+public class CarryCabinRegisterRequestVo {
+
+    private String ip;
+}
