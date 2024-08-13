0a307da0430d6b3986207507206ceee6d538461c
ddolboghi <rhdfyd128@gmail.com>
2024-08-13 10:22:57 +0900

    fix: cosy house API 수정

--- src/main/java/site/cleanfree/be_main/cozyhouse/presentation/CozyHouseController.java
+++ src/main/java/site/cleanfree/be_main/cozyhouse/presentation/CozyHouseController.java
@@ -1,6 +1,5 @@
 package site.cleanfree.be_main.cozyhouse.presentation;
 
-import jakarta.servlet.http.HttpServletRequest;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.ResponseEntity;
@@ -9,7 +8,6 @@ import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 import site.cleanfree.be_main.common.BaseResponse;
-import site.cleanfree.be_main.common.ClientIpAccessor;
 import site.cleanfree.be_main.cozyhouse.application.CozyHouseService;
 import site.cleanfree.be_main.cozyhouse.vo.CozyHouseRegisterRequestVo;
 
@@ -23,10 +21,8 @@ public class CozyHouseController {
 
     @PostMapping("/register")
     public ResponseEntity<BaseResponse<Object>> register(
-        HttpServletRequest request,
         @RequestBody CozyHouseRegisterRequestVo cozyHouseRegisterRequestVo
     ) {
-        String clientIp = ClientIpAccessor.getIp(request);
-        return ResponseEntity.ok(cozyHouseService.register(clientIp, cozyHouseRegisterRequestVo));
+        return ResponseEntity.ok(cozyHouseService.register(cozyHouseRegisterRequestVo));
     }
 }
