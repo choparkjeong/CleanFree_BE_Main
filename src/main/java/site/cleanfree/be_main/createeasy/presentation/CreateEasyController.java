7873aff49fe9acd2f299d61b609dd607f14bb38c
ddolboghi <rhdfyd128@gmail.com>
2024-08-13 10:02:44 +0900

    fix: CreateEasy API URI 수정

--- src/main/java/site/cleanfree/be_main/createeasy/presentation/CreateEasyController.java
+++ src/main/java/site/cleanfree/be_main/createeasy/presentation/CreateEasyController.java
@@ -2,7 +2,6 @@ package site.cleanfree.be_main.createeasy.presentation;
 
 import io.swagger.v3.oas.annotations.Operation;
 import io.swagger.v3.oas.annotations.tags.Tag;
-import jakarta.servlet.http.HttpServletRequest;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -10,12 +9,11 @@ import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 import site.cleanfree.be_main.common.BaseResponse;
-import site.cleanfree.be_main.common.ClientIpAccessor;
-import site.cleanfree.be_main.createeasy.vo.CreateEasyRegisterRequestVo;
 import site.cleanfree.be_main.createeasy.application.CreateEasyService;
+import site.cleanfree.be_main.createeasy.vo.CreateEasyRegisterRequestVo;
 
 @RestController
-@RequestMapping("/api/v1/createvalue")
+@RequestMapping("/api/v1/createeasy")
 @RequiredArgsConstructor
 @Tag(name = "create easy API", description = "인플루언서 컨텐츠 기획 API")
 public class CreateEasyController {
