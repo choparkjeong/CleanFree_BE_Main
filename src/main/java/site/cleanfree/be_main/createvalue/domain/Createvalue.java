04d12db261a964c53ada4dd5e21d391ad16c7bde
ddolboghi <rhdfyd128@gmail.com>
2024-08-13 10:03:06 +0900

    fix: CreateValue 컬렉션 명 변경

--- src/main/java/site/cleanfree/be_main/createvalue/domain/Createvalue.java
+++ src/main/java/site/cleanfree/be_main/createvalue/domain/Createvalue.java
@@ -3,12 +3,11 @@ package site.cleanfree.be_main.createvalue.domain;
 import jakarta.persistence.Id;
 import lombok.Builder;
 import lombok.Getter;
-import org.springframework.data.mongodb.core.index.Indexed;
 import org.springframework.data.mongodb.core.mapping.Document;
 import site.cleanfree.be_main.common.MongoBaseTimeEntity;
 
 @Getter
-@Document(collection = "createvalue")
+@Document(collection = "create_value")
 public class Createvalue extends MongoBaseTimeEntity {
 
     @Id
